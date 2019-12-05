84ccc60c1e1b38fdbfeea7813e6c6788f3fc7bf5
pg30 <pranaygarg001@gmail.com>
2019-12-05 15:14:28 +0530

    Taxi less than 6 passenger layout added

---
+++ app/src/main/java/com/example/premiumcalculator/taxilessthan6.java
@@ -0,0 +1,14 @@
+package com.example.premiumcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class taxilessthan6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taxilessthan6);
+    }
+}
