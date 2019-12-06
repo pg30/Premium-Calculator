948e6aa6880a5312ae06b34cf50a0644e2232ab2
pg30 <pranaygarg001@gmail.com>
2019-12-06 08:35:12 +0530

    two wheeler breakup layout added

---
+++ app/src/main/java/com/example/premiumcalculator/two_wheeler_breakup.java
@@ -0,0 +1,14 @@
+package com.example.premiumcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class two_wheeler_breakup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_wheeler_breakup);
+    }
+}
